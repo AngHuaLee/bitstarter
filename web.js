c666d8695a2ef5b1fb2d193a74e198a0e5929ee5
Ubuntu <ubuntu@ip-172-31-41-48.us-west-2.compute.internal>
2013-07-09 14:31:55 +0000

    Updated grader.js and test file

--- web.js
+++ web.js
@@ -3,9 +3,19 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 var txt = fs.readFileSync('index.html');
+var test1 = fs.readFileSync('test1.html');
+var test2 = fs.readFileSync('test2.html');
 
 app.get('/', function(request, response) {
   response.send(txt.toString());
+});
+
+app.get('/test1.html', function(request, response) {
+  response.send(test1.toString());
+});
+
+app.get('/test2.html', function(request, response) {
+  response.send(test2.toString());
 });
 
 var port = process.env.PORT || 5000;
